80dd1d095007ae7a7474c2407c80905ae4d20a79
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-04-29 20:22:19 +0200

    create test: a year is a leap year if divisible by 4

--- src/test/java/com/codurance/katalyst/YearTest.java
+++ src/test/java/com/codurance/katalyst/YearTest.java
@@ -1,6 +1,7 @@
 package com.codurance.katalyst;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -10,5 +11,11 @@ public class YearTest {
     public void a_year_is_not_a_leap_year_if_not_divisible_by_4(){
         Year year = Year.create(1997);
         assertFalse(year.isLeapYear());
+    }
+
+    @Test
+    public void a_year_is_a_leap_year_if_divisible_by_4(){
+        Year year = Year.create(1996);
+        assertTrue(year.isLeapYear());
     }
 }
