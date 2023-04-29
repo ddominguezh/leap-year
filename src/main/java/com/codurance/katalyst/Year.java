854bdcf28198f4b508f2f31f884630e0164cb5a4
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-04-29 20:40:28 +0200

    fix test: A year is not a leap year if divisible by 100 but not by 400

--- src/main/java/com/codurance/katalyst/Year.java
+++ src/main/java/com/codurance/katalyst/Year.java
@@ -10,6 +10,9 @@ public class Year {
         return new Year(year);
     }
     public boolean isLeapYear() {
+        if(value % 100 == 0 && value % 400 != 0){
+            return false;
+        }
         return value % 4 == 0;
     }
 }
